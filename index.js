dc67635a3b592d67c954365f00b428ddad24c900
Lundy Orlando <simplyeazy@gmail.com>
2018-09-14 11:06:20 +0700

    Release 0 Read Multiple Data and Manipulate Object using Callback

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const fs = require('fs');
 const sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // Code here
+function match_data(parentFile, childrenFile, callback) {
+  const jsonParentData = fs.readFileSync(parentFile);
+  sleep.sleep(5);
+  const jsonChildData = fs.readFileSync(childrenFile);
+  sleep.sleep(5);
+  let parentData = JSON.parse(jsonParentData);
+  let childData = JSON.parse(jsonChildData);
+  for (let i = 0; i < parentData.length; i++) {
+    parentData[i].children = [];
+    for (let j = 0; j < childData.length; j++) {
+      if (parentData[i].last_name === childData[j].family) {
+        parentData[i].children.push(childData[j].full_name);
+      }
+    }
+  }
+  callback(parentData);
 }
 
-match_data('./parents.json', './children.json')
+match_data('./parents.json', './children.json', function (parentData) {
+  console.log(parentData);
+})
 console.log("Notification : Data sedang diproses !");
